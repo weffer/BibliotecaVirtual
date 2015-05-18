14b23d3c0725e654243ccaffa874880fae9ebc9a
jore8 <jore@3devnet.com>
2015-05-17 19:44:43 -0500

    Primer Commit

---
+++ BibliotecaVirtual/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BibliotecaVirtual.MvcApplication" Language="C#" %>
---
+++ BibliotecaVirtual/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BibliotecaVirtual.Startup))]
+namespace BibliotecaVirtual
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
