b4145775d452b77c1b9aac8e1f01f8b3d9a77568
weffer <jeffer.ore@gmail.com>
2015-05-24 12:48:48 -0500

    cambios

--- BibliotecaVirtual.Common/LibroCreateViewModel.cs
+++ BibliotecaVirtual.Common/LibroCreateViewModel.cs
@@ -1,15 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace BibliotecaVirtual.Dominio
+namespace BibliotecaVirtual.Common
 {
-    [Table("Libro")]
-    public class Libro : EntityBase
+    public class LibroCreateViewModel
     {
         public string nombre { get; set; }
         public string descripcion { get; set; }
         public string nombre_autor { get; set; }
         public string genero { get; set; }
-        public Categoria categoria { get; set; }
         public int categoriaid { get; set; }
     }
 }
