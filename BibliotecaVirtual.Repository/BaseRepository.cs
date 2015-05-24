521ca07091a5750de760be46e9364d29f6036389
weffer <jeffer.ore@gmail.com>
2015-05-24 11:29:55 -0500

    ultimos cambios 2

---
+++ BibliotecaVirtual.Repository/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using BibliotecaVirtual.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.Repository
+{
+    public class BaseRepository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : EntityBase
+        where TContext : DbContext
+    {
+
+        protected TContext Context;
+
+        public BaseRepository(TContext Context)
+        {
+            this.Context = Context;
+        }
+
+        public IQueryable<TEntity> Get()
+        {
+            return Context.Set<TEntity>().AsQueryable();
+        }
+
+        public void Create(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+            Context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context = null;
+        }
+    }
+}
---
+++ BibliotecaVirtual.Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using BibliotecaVirtual.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.Repository
+{
+    public interface IRepository<TEntity> : IDisposable
+        where TEntity : EntityBase
+    {
+        IQueryable<TEntity> Get();
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
