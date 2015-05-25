475284c704db4ddd38fd203cd0001261bc3995ea
jore8 <jore@3devnet.com>
2015-05-25 17:37:54 -0500

    ingresando librerias de javascript

---
+++ BibliotecaVirtual.AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BibliotecaVirtual.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get
+            {
+                return "DomainToViewModelMappings";
+            }
+        }
+
+        protected override void Configure()
+        {
+          
+        }
+    }
+}
