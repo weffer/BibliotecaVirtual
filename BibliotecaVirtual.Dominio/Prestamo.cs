c0ffb3a86d4120c226b3cbe1b3822b82d5a7b3fc
jore8 <jore@3devnet.com>
2015-07-05 07:35:37 -0500

    a

---
+++ BibliotecaVirtual.Dominio/Prestamo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.Dominio
+{
+    [Table("Prestamo")]
+    public class Prestamo : EntityBase
+    {
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; }
+
+        public int IdLibro { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? FechaPrestamo { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? FechaDevolucion { get; set; }
+
+        [StringLength(1)]
+        public string Devuelto { get; set; }
+
+        public virtual Libro Libro { get; set; }
+    }
+}
