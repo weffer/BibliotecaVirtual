e0bc26cbbbce7e576e3c416491a7f8bf903a7360
jore8 <jore@3devnet.com>
2015-07-05 07:36:09 -0500

    h

---
+++ BibliotecaVirtual.Common/Prestamo/PrestamoCreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.Common.Prestamo
+{
+    public class PrestamoCreateViewModel
+    {        
+        [Display(Name = "Usuario")]
+        public string Username { get; set; }
+             
+        [Display(Name = "Fecha de Prestamo")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime FechaPrestamo { get; set; }                
+        public DateTime FechaDevolucion { get; set; }
+
+        [Required(ErrorMessage = "Ingrese la cantidad de dias")]
+        [Display(Name = "Cantidad de dias")]
+        public int? DiasPrestamo { get; set; }
+        public int IdLibro { get; set; }
+        public string Devuelto { get; set; }
+    }
+}
